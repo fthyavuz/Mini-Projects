c26d79db870cb47493d09da158765f969f0855d5
Fatih Yavuz <ibrahim.fatih.yavuz@gmail.com>
2024-01-26 15:00:40 +0100

    Add feature: display dummy data when tokens are unavailable

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,28 +1,31 @@
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+const messageThree = document.querySelector("#message-3");
 
-weatherForm.addEventListener('submit',(e)=>{
-    e.preventDefault()
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const location = search.value
-    const endpoint = '/weather?address=' + location
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+  const location = search.value;
+  const endpoint = "/weather?address=" + location;
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+  messageThree.textContent = "";
 
-    fetch(endpoint).then((response)=>{
-    response.json().then((data)=>{
-        if(data.errorMessage){
-            messageOne.textContent = data.errorMessage 
-        } else {
-            messageOne.textContent = data.location 
-            messageTwo.textContent = data.forecast 
-
-        } 
-    })
-})
-
-})
-
-
+  fetch(endpoint).then((response) => {
+    response.json().then((data) => {
+      if (data.errorMessage) {
+        messageOne.textContent = data.errorMessage;
+      } else if (!data.isTokenAvaliable) {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        messageThree.textContent =
+          "Attention: Tokens have not been provided in the dev.env file. As a result, the weather information returned is dummy data, and the location has not been verified";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,108 +1,116 @@
-const express = require('express')
-const path = require('path')
-const hbs  = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const checkTokens = require("./utils/checkTokens");
+const e = require("express");
 
-const app = express()
+const app = express();
 
 // define port
-//const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const partialPath = path.join(__dirname,'../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine','hbs')
-app.set('views',viewsPath)
-hbs.registerPartials(partialPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 // handle index file
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Weather',
-        name:'Fatih Yavuz'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Fatih Yavuz",
+  });
+});
 
 // handle help page
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        helpText:'Some help text',
-        title: 'Help',
-        name: 'Fatih Yavuz'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "Some help text",
+    title: "Help",
+    name: "Fatih Yavuz",
+  });
+});
 
 // handle about page
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About Me',
-        name: 'Fatih Yavuz'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Fatih Yavuz",
+  });
+});
 
 // handle weather data
-app.get('/weather',(req,res)=>{
-
-    const address = req.query.address
-    if(!address){
+app.get("/weather", (req, res) => {
+  const isTokenAvaliable = checkTokens();
+  const address = req.query.address;
+  if (!address) {
+    return res.send({
+      errorMessage: "You must provide an address!",
+    });
+  }
+  if (isTokenAvaliable) {
+    geocode(address, (error, { latitude, longtitude, placeName } = {}) => {
+      if (error) {
         return res.send({
-            errorMessage:'You must provide an address!'
-        })
-    }
+          errorMessage: error,
+        });
+      }
 
-    geocode(address,(error,{latitude,longtitude,placeName}={})=>{
-        if (error){
-            return res.send({
-                errorMessage: error
-            })  
+      forecast(latitude, longtitude, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            errorMessage: error,
+          });
         }
-    
-        forecast(latitude,longtitude,(error,forecastData)=>{
-            if(error){
-                return res.send({
-                    errorMessage:error
-                }) 
-            }
-
-            res.send({
-                forecast: forecastData,
-                location: placeName,
-                address:  address
-            })
-
-        })
-    })
-})
-
-// handle 404 pages 
-
-app.get('/help/*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        name: 'Fatih Yavuz',
-        errorMessage:'Help article not found'
-        
-    })
-})
+        res.send({
+          forecast: forecastData,
+          location: placeName,
+          address: address,
+          isTokenAvaliable: isTokenAvaliable,
+        });
+      });
+    });
+  } else {
+    res.send({
+      forecast:
+        "Partly cloudy .It is currently 6 degrees out. It feels like 4 degrees out.Air humidity 81",
+      location: address,
+      address: address,
+      isTokenAvaliable: isTokenAvaliable,
+    });
+  }
+});
 
 // handle 404 pages
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title:404,
-        name: 'Fatih Yavuz',
-        errorMessage:'Page Not Found!'
-        
-    })
-})
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Fatih Yavuz",
+    errorMessage: "Help article not found",
+  });
+});
+
+// handle 404 pages
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: 404,
+    name: "Fatih Yavuz",
+    errorMessage: "Page Not Found!",
+  });
+});
 
 // start the server
-app.listen(process.env.PORT,()=>{
-    console.log('The server is up on port ' + process.env.PORT)
-})
+app.listen(port, () => {
+  console.log("The server is up on port " + port);
+});
